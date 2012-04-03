dc06488fcc32c6d6954a7c8af716c4fb5346c380
ahmontero <antonio.hinojo.montero@gmail.com>
2012-04-03 09:08:33 +0200

    Added some javascript

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+function createPyramid(x, y){
+        $('#pyramid').Pyramid({
+              'height': 350,
+              'width': 350,
+              'slices':4,
+              'slice_separation': 0.1,
+              'colours': ['red', 'yellow', 'green', 'blue'],
+              'text': ['1', '2', '3', '4']
+             })
+             .on('click', function(event){ alert(event.text) });
+};
+
